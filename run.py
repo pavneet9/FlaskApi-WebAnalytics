68e6f060f3832e69e996110c5f5fba060d2d5e7d
pavneet9 <pavneet992@gmail.com>
2020-06-01 22:59:52 +0000

    All files

--- run.py
+++ run.py
@@ -1,10 +1,27 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from resources.routes import initialize_routes
+from flask_restful import Api
+from flask_bcrypt import Bcrypt
+from database.db import inititalize_db
+from resources.errors import errors
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}},methods=['POST', 'GET','OPTIONS'], send_wildcard=True)
+db = inititalize_db(app)
+api = Api(app)
+api = Api(app, errors=errors)
+bcrypt = Bcrypt(app)
+app.secret_key = 'pavneet' 
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
-@app.route('/', methods=['GET'])
-def hello():
-    return  jsonify({'hello': 'world'})
+jwt = JWTManager(app)
+initialize_routes(api)
 
 
-app.run(host='0.0.0.0',port='5000')
+if __name__== "__main__":
+	db.init_app(app)
+	app.run(host='0.0.0.0',port='5000')
