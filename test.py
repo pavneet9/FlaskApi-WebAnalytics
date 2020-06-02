118cef1e06d7c0d8836c167c77db4bb44219a10d
pavneet9 <pavneettiwana90@gmail.com>
2020-06-02 11:01:38 -0600

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-
-from kafka import KafkaProducer
-
-cluster_name = "35.226.38.97" # cluster in which kafka & zookeeper are installed  
-
-producer = KafkaProducer(bootstrap_servers=[cluster_name+'-m:9092'],value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-
